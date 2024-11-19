6a11e0772d53cf3e4c20ef241cfc80b3898d6333
brandscape <wiskim08@gmail.com>
2024-11-19 15:37:52 +0900

    메인 모바일버튼추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,12 @@ import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
 import { keywordStr } from "./search/type";
 import { useSetRecoilState } from "recoil";
-import { filterOptionState, searchKeywordState } from "@/recoil/search/search-atom";
+import {
+  filterOptionState,
+  searchKeywordState,
+} from "@/recoil/search/search-atom";
 import { isApplicationNumber } from "@/utils";
+import Link from "next/link";
 
 export default function Home() {
   const id = useId();
@@ -25,7 +29,8 @@ export default function Home() {
       if (searchInput instanceof HTMLInputElement) {
         if (searchInput.value) {
           const keywordValues = localStorage.getItem(keywordStr);
-          const searchKeywords = keywordValues === null ? [] : keywordValues.split(",");
+          const searchKeywords =
+            keywordValues === null ? [] : keywordValues.split(",");
 
           const keywords = [searchInput.value, ...searchKeywords];
           localStorage.setItem(keywordStr, keywords.slice(0, 8).toString());
@@ -34,8 +39,14 @@ export default function Home() {
           setFilterOptions((prev) => ({
             ...prev,
             ...(isApplicationNumber(searchInput.value)
-              ? { applicationNumber: searchInput.value, trademarkName: undefined }
-              : { trademarkName: searchInput.value, applicationNumber: undefined }),
+              ? {
+                  applicationNumber: searchInput.value,
+                  trademarkName: undefined,
+                }
+              : {
+                  trademarkName: searchInput.value,
+                  applicationNumber: undefined,
+                }),
           }));
 
           router.push(`/search?s=${searchInput.value}`);
@@ -50,7 +61,7 @@ export default function Home() {
   );
 
   return (
-    <main className="min-h-screen w-full p-24 xs:px-10 lg:px-[6.5rem] main-background flex items-center justify-center">
+    <main className="min-h-screen w-full p-24 xs:px-10 lg:px-[6.5rem] main-background flex items-center justify-center sm:p-[6rem_6rem_15rem_6rem]">
       <div className="container flex flex-col flex-nowrap gap-[1.875rem] justify-center items-center">
         <div className="logo">
           <Image src={Logo} alt="logo" width={428} height={90} priority />
@@ -61,6 +72,11 @@ export default function Home() {
           </form>
         </div>
       </div>
+      <Link href="https://ip.brandscape.co.kr/%EC%83%81%ED%91%9C%EB%93%B1%EB%A1%9D">
+        <button className="fixed bottom-0 left-0 w-full px-6 py-4 text-lg font-bold text-white bg-[#1a4db6] sm:block hidden">
+          상표등록 문의하기
+        </button>
+      </Link>
     </main>
   );
 }
